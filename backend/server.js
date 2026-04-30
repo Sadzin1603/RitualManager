47e80100dfa9bf947c5ab29acd0a012d7bee2252
Sadzin1603 <kauanfeliperossi@gmail.com>
2026-04-30 20:17:24 -0300

    feat: implementado hash senha, login com jwt

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
-import 'dotenv/config'; // Mova para a primeira linha
+import 'dotenv/config'; 
 import express from "express";
 import { createClient } from "@supabase/supabase-js";
-import cors from "cors"
+import cors from "cors";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import authToken from"./middleware.js"
 
 const port = 3000
+
 export const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_KEY)
 const app = express();
 app.use(express.json())
 app.use(cors());
 
-app.post('/cadastro',async(req,res)=>{
+//GET -pegar
+//POST -criar
+//DELETE -deleta
+//PUT -updeita
+
+
+app.post('/cadastro',async (req,res)=>{
 
     const {name,email,password} = req.body
 
@@ -17,9 +27,11 @@ app.post('/cadastro',async(req,res)=>{
         return res.status(400).json({error:'Email e senha obrigatorios'})
     }
 
+    const passHash = await bcrypt.hash(password, 12);
+
     const {data,error} = await supabase
                     .from('Clients')
-                    .insert([{name,email,password}])
+                    .insert([{name,email,password:passHash}])
     
     if(error){
         return res.status(500).json({error:error.message})
@@ -28,6 +40,52 @@ app.post('/cadastro',async(req,res)=>{
     return res.status(201).json({message:"Cliente salvo com sucesso",data})
 
 })
+
+app.post("/login",async (req,res)=>{
+    const {email,password} = req.body
+    const {data,error} = await supabase
+                        .from("Clients")
+                        .select("*")
+                        .eq("email",email)
+                        .single()
+
+                        
+    if(error){
+        return res.status(500).json({error:error.message, user:"User Not Found"})
+    }
+    
+    const senhaValida = await bcrypt.compare(password, data.password);
+    
+
+    if(!senhaValida){
+        return res.status(400).json({error:"Senha invalida"})
+    }
+
+    const token = jwt.sign(
+    { id: data.id },
+    process.env.JWT_SECRET,
+    { expiresIn: "10m" }
+    );
+
+    res.status(200).json({ token });
+})
+
+app.get("/protegida", authToken,(req,res)=>{
+    res.json({message:"Você pode estar aq"})
+})
+
+
+app.get('/cadastros',async(req,res)=>{
+    const {data,error} = await supabase
+                    .from('Clients')
+                    .select('*')
+    if(error){
+        return res.status(500).json({error:error.message})
+    }
+
+    return res.status(201).json({message:"Clientes resgatados com sucesso",data})
+})
+
 
 
 
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,14 +1,44 @@
 import { useState } from "react"
 import { EyeIcon, EyeOffIcon } from "lucide-react";
+import {useNavigate} from "react-router-dom"
 import Input from "../components/Input"
 
 function Login(){
+    const navigate = useNavigate();
+
     const [email,setEmail] = useState<String>("")
     const [password,setPassword] = useState<String>("")
 
     const [show, setShow] = useState<boolean>(false);//ver a senha
     function mostrar() {
         setShow(!show);
+    }
+    
+    
+    async function logar(){
+        try{
+            const response = await fetch("http://localhost:3000/login",{
+                method:"POST",
+                headers:{ 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email,password })
+            }) 
+            const data = await response.json();
+            localStorage.setItem("token",data.token)
+        }catch(e){
+            console.log(e)
+        }
+    }
+    async function teste(){
+        
+        const token = localStorage.getItem("token");
+        if(token){
+            await fetch("http://localhost:3000/protegida",{
+                headers:{Authorization: `Bearer ${token}`}
+            })
+           navigate("/") //MUDAR A ROTA PARA ONDE EU QUERO IR
+        }
+        
+    
     }
 
     return( 
@@ -43,9 +73,20 @@ function Login(){
                 <button
                     className="input px-4 py-2 rounded-2xl placeholder-gray-400 text-white cursor-pointer"
                     type="submit"
-                    
+                    onClick={
+                        logar
+                    }
                 >
                     Logar
+                </button>
+                <button
+                    className="input px-4 py-2 rounded-2xl placeholder-gray-400 text-white cursor-pointer"
+                    type="submit"
+                    onClick={
+                        teste
+                    }
+                >
+                    acessar o desconhecido
                 </button>
             </div>
         </div>
