47dc3ef45f65b83d6e577c79d5fc0725d3acae61
Sadzin1603 <kauanfeliperossi@gmail.com>
2026-05-14 09:49:37 -0300

    feat: modal para as pages e copiar ritual

---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,42 @@
+function Modal({isOpen,title,message,onConfirm,onCancel}) {
+
+    if (!isOpen) return null
+
+    return (
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
+            
+            <div className="bg-zinc-900 p-6 rounded-xl w-96">
+
+                <h2 className="text-xl font-bold mb-2">
+                    {title}
+                </h2>
+
+                <p className="text-zinc-300 mb-6">
+                    {message}
+                </p>
+
+                <div className="flex justify-end gap-2">
+
+                    <button
+                        onClick={onCancel}
+                        className="px-4 py-2 bg-zinc-700 rounded"
+                    >
+                        Cancelar
+                    </button>
+
+                    <button
+                        onClick={onConfirm}
+                        className="px-4 py-2 bg-red-600 rounded"
+                    >
+                        Confirmar
+                    </button>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Modal
--- frontend/src/pages/Ritual.tsx
+++ frontend/src/pages/Ritual.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import Modal from '../components/Modal'
 import "./Ritual.css";
 
 export default function Ritual() {
@@ -21,6 +23,48 @@ export default function Ritual() {
     fetchData();
   }, []);
 
+
+  const [open,setOpen] = useState(false)
+  async function copiarRitual(){
+      const token = localStorage.getItem("token");
+      const decoded = jwtDecode(token!);
+
+      const formData = new FormData();
+      formData.append("name", `${ritual.name} (cópia)`);
+      formData.append("element",    ritual.element);
+      formData.append("circle",     ritual.circle);
+      formData.append("exec",       ritual.exec);
+      formData.append("range", ritual.alcance);
+      formData.append("duration", ritual.duracaoInput);
+      formData.append("area", ritual.area);
+      formData.append("target", ritual.target);
+      formData.append("effect", ritual.effect);
+      formData.append("resistence", ritual.resistence);
+      formData.append("dices", ritual.dices);
+      formData.append("description", ritual.description);
+      formData.append("discent_description", ritual.discent_description);
+      formData.append("truly_description", ritual.truly_description);
+      formData.append("discent_dices", ritual.discent_dices);
+      formData.append("truly_dices", ritual.truly_dices)
+      formData.append("status", "pendente");
+      formData.append("creator",    decoded.id);
+
+      // A imagem já é uma URL — passa ela como string se o backend aceitar,
+      // ou faz fetch da URL pra virar um Blob antes d
+      // e appender como File
+      const res = await fetch(ritual.img);
+      const blob = await res.blob();
+      formData.append("file", new File([blob], "ritual_img.jpg", { type: blob.type }));
+      //formData.append("img", ritual.img); // ou o blob
+
+      await fetch("http://localhost:3000/ritual", {
+        method: "POST",
+        headers: { Authorization: `Bearer ${token}` },
+        body: formData,
+      });
+      setOpen(false)
+      navigate("/profile")
+  }
   return (
     <div
       className="pagina"
@@ -52,19 +96,26 @@ export default function Ritual() {
                 Criado por {ritual.creator?.name}
               </p>
             </div>
-
             <div className="absolute right-0">
+              {/*   Botão de editar   */}
               <button className="absolute top-[-1rem] right-[1.5rem] bg-[#18181B] rounded-lg" onClick={()=>navigate("/rituais",{state:ritual})} >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} color="green" stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
               </svg>
               </button>
-
+              {/*   Botão de copiar   */}
               <button className="absolute top-[-1rem] right-[-1.5rem] bg-[#18181B] rounded-lg" >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6" onClick={()=>setOpen(true)}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 8.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v8.25A2.25 2.25 0 0 0 6 16.5h2.25m8.25-8.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-7.5A2.25 2.25 0 0 1 8.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 0 0-2.25 2.25v6" />
                 </svg>
               </button>
+              <Modal
+                  isOpen={open}
+                  title="Copiar Ritual"
+                  message="Tem certeza que deseja copiasr esse ritual?"
+                  onConfirm={copiarRitual}
+                  onCancel={() => setOpen(false)}
+              />
             </div>
           </div>
         </div>
